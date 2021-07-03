a05f93c545c134c29783a130b3c19a55ed896aba
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-07-03 23:03:40 +0900

    210628 실습문제

---
+++ 2021 여름 - 프로그래밍 기초/Lab5/2.c
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int arr[5][4] = {
+		{ 97,90,88,95 },
+		{ 76,89,75,83 },
+		{ 60,70,88,82 },
+		{ 83,89,92,85 },
+		{ 75,73,72,78 }
+	};
+	int sum1 = 0;
+	int sum2 = 0;
+	int all = 0;
+
+	for (int i = 0; i < 5; i++)
+	{
+		
+	}
+
+	return 0;
+}
