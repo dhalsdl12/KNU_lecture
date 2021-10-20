a2f9a4067c695749b327ceede927bc6b0cfdc555
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2021-10-20 10:07:59 +0900

    Add files via upload

---
+++ 2021-2/시스템프로그래밍/210923/find01.c
@@ -0,0 +1,41 @@
+﻿#include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+
+void find(char* name)
+{
+	char name2[1000];
+	DIR* dp;
+	struct dirent* dent;
+
+	if ((dp = opendir(name)) == NULL)
+	{
+		return;
+	}
+
+	while ((dent = readdir(dp)) != NULL) {
+		if (strcmp(dent->d_name, "..") == 0 || strcmp(dent->d_name, ".") == 0)
+			continue;
+
+		strcpy(name2, name);
+		strcat(name2, "/");
+		strcat(name2, dent->d_name);
+		printf("%s\n", name2);
+
+		find(name2);
+	}
+
+	closedir(dp);
+}
+int main(int ac, char* av[])
+{
+	printf("%s\n", av[1]);
+	find(av[1]);
+
+	return 0;
+}
---
+++ 2021-2/시스템프로그래밍/210930/setuc.c
@@ -0,0 +1,29 @@
+﻿//author: 2018115425 권오민
+//datetime: 2021-09-30 11:53
+
+#include 	<stdio.h>
+#include	<termios.h>
+#include	<stdlib.h>
+
+#define oops(s,x) { perror(s); exit(x); }
+
+int main(int ac, char* av[])
+{
+	struct termios info;
+
+	if (ac == 1)
+		exit(0);
+
+	if (tcgetattr(0, &info) == -1)		/* get attribs */
+		oops("tcgetattr", 1);
+
+	if (av[1][0] == 'y')
+		info.c_oflag |= OLCUC;		/* turn on bit */
+	else
+		info.c_oflag &= ~OLCUC;		/* turn off bit */
+
+	if (tcsetattr(0, TCSANOW, &info) == -1)	/* set attribs */
+		oops("tcsetattr", 2);
+
+	return 0;
+}
